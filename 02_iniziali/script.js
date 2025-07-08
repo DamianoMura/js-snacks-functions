43358f0ee5e6e7c75c42bb0abac12a3c564e17a6
DamianoMura <mura.damiano14@gmail.com>
2025-07-08 16:28:46 +0200

    [upd] comments snack 2

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -4,16 +4,20 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+//la funzione avrà in ingresso un' array di nomi
 function getInitials(array){
+  //dichiaro l'array iniziali
 let initials=[];
   for (let i=0; i<array.length ; i++){
+
 initials[i]=array[i].charAt(0);
   }
+  //restituiamo l'array con le iniziali
 return initials;
 }
 
 // Invoca la funzione qui e stampa il risultato in console
 
-console.log(getInitials(names))
+console.log(getInitials(names))//invoco direttamente la funzione nel console log in quanto mi restituisce direttamente l'array con le iniziali
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
